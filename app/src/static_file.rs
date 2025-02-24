59162cfe4aa09dff7fa9a6d5ac59d30c8cd294ac
Jeff Mitchell <sentinel1909@jeff-mitchell.dev>
2025-02-24 07:43:46 -0800

    bug: fix paths in static file serving constructor

--- app/src/static_file.rs
+++ app/src/static_file.rs
@@ -8,15 +8,11 @@ use pavex::response::Response;
 use std::borrow::Cow;
 use std::fs::File;
 use std::io::Read;
-use std::path::{Path, PathBuf};
-
-// base directory for serving static files
-const STATIC_DIR: &str = "/home/jeff/dev/source/repos/rust-lang/pavex-files";
+use std::path::Path;
 
 // struct type to represent a static asset, CCSS, JS, an image, or anything else
 #[derive(Debug, Clone)]
 pub struct StaticFile {
-    pub asset_path: PathBuf,
     pub asset_data: Vec<u8>,
     pub asset_mime_type: Cow<'static, str>,
 }
@@ -30,7 +26,8 @@ pub async fn io_error2response(e: &pavex::Error) -> Response {
 impl StaticFile {
     pub async fn new(request_head: &RequestHead) -> Result<Self, std::io::Error> {
         let request_path = Path::new(request_head.target.path().trim_start_matches("/"));
-        let full_path = Path::new(STATIC_DIR).join(request_path);
+        let base_dir = std::env::current_dir()?;
+        let full_path = Path::new(&base_dir).join(request_path);
         let mut file = File::open(&full_path)?;
         let mut contents = Vec::new();
         file.read_to_end(&mut contents)?;
@@ -41,7 +38,6 @@ impl StaticFile {
             .into();
 
         let static_asset = Self {
-            asset_path: full_path,
             asset_data: contents,
             asset_mime_type: mime_type,
         };
